870fc6bebc048a935f7580ab53c4b77a50d3f283
Abhinav <raaviabhinav93@gmail.com>
2026-08-20 23:34:01 +0530

    adding files

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from valiadations import Valiadations
 from model import predict_price
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="car-price-prediction")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later you can restrict to your streamlit domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/",response_model=str)
 def home():
